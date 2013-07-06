ea169d3b8896cf73a4387d2fa855345ae37d6d6c
Ubuntu <ubuntu@ip-172-31-46-133.us-west-2.compute.internal>
2013-07-06 11:49:32 +0000

    origneeel voorbeeld

--- web.js
+++ web.js
@@ -1,14 +1,12 @@
-var express = require("express");
-var app = express();
-app.use(express.logger() );
+var express = require('express');
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('dit werkt wel');
-  }
-);
+  response.send('Hello World!');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
