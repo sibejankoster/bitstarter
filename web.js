f9e4a8c7b8095cfc1aae2374ea80a27e2cfd9a9f
Ubuntu <ubuntu@ip-172-31-46-133.us-west-2.compute.internal>
2013-07-06 11:58:06 +0000

    originele versie met js gecorr

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require ('fs');
 
 var app = express.createServer(express.logger());
 
-frs.readFile('/index.html', function (err, data) {
+fs.readFile('/index.html', function (err, data) {
   if (err) {
      throw err;
    }
